8663ed54d0802c34620d39e1e2cecb65c8fff63e
mikezalik <mike.zalik@gmail.com>
2020-03-04 11:03:18 -0800

    Adds note edit activity and intent

---
+++ app/src/main/java/com/mikezalik/NoteEditActivity.java
@@ -0,0 +1,16 @@
+package com.mikezalik;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mikezalik.notes.R;
+
+public class NoteEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_edit);
+    }
+}
--- app/src/main/java/com/mikezalik/notes/MainActivity.java
+++ app/src/main/java/com/mikezalik/notes/MainActivity.java
@@ -2,9 +2,14 @@ package com.mikezalik.notes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import com.mikezalik.NoteEditActivity;
 
 import java.util.ArrayList;
 
@@ -18,7 +23,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ListView listView = findViewById(R.id.listView);
-        notes.add("Exmaple Note");
+        notes.add("Example Note");
         ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, notes);
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(getApplicationContext(), NoteEditActivity.class);
+                intent.putExtra("noteId", i);
+                startActivity(intent);
+            }
+        });
     }
 }
