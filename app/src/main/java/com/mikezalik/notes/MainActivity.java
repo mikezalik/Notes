3f3f7961851afb0f60100cf31f242f8c58042025
mikezalik <mike.zalik@gmail.com>
2020-03-04 10:28:53 -0800

    Adds array list, list view and array adapter

--- app/src/main/java/com/mikezalik/notes/MainActivity.java
+++ app/src/main/java/com/mikezalik/notes/MainActivity.java
@@ -3,12 +3,22 @@ package com.mikezalik.notes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> notes = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.listView);
+        notes.add("Exmaple Note");
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, notes);
     }
 }
